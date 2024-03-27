b64bb629d3b7f4988c3025dfb8cfee188ac49eb3
guillebags <guillebags@gmail.com>
2024-03-27 13:55:12 +0100

    interfaces, generic parameters

--- exercise_files/Ch02/02_06b/src/demo.ts
+++ exercise_files/Ch02/02_06b/src/demo.ts
@@ -1,11 +1,11 @@
 interface Contact {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
 function clone(source: Contact): Contact {
-    return Object.apply({}, source);
+  return { ...source };
 }
 
 const a: Contact = { id: 123, name: "Homer Simpson" };
-const b = clone(a)
+const b = clone(a);
